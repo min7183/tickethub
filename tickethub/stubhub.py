33a168ea7aeea1907573b3fe3f34cb7a6e806e5c
min7183 <106830933+min7183@users.noreply.github.com>
2024-08-30 23:18:36 -0500

    made some modifications to stubhub.py

--- tickethub/stubhub.py
+++ tickethub/stubhub.py
@@ -6,3 +6,11 @@ import os
 stem = ""
 load_dotenv()
 key = os.getenv("STUBHUB_KEY")
+
+def get(url):
+    print(url)
+    resp = requests.get(url)
+    print(resp)
+    return resp.json()
+
+def get_event()
