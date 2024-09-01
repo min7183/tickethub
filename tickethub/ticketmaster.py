e2f1b642674c215723e045142b72fc55189be0dd
Emily Tom <emilytom808@gmail.com>
2024-08-31 15:43:44 -1000

    added get eventlist and get event function, get eventlist returns list of events based on location (date not working yet, iso8601 format needed)

--- tickethub/ticketmaster.py
+++ tickethub/ticketmaster.py
@@ -1,20 +1,35 @@
 from dotenv import load_dotenv
 import os
 
+from datetime import datetime
+
 from urllib import parse
 import requests
-
+from pprint import pprint
+    
 load_dotenv()
-
-api_key = os.getenv('TICKETMASTER_KEY')
+key = os.getenv('TICKETMASTER_KEY')
 event_id = -1
+stem="https://app.ticketmaster.com/discovery/v2/events"
 
-base_url="https://app.ticketmaster.com/discovery/v2/events"
-url=f'{base_url}/{event_id}.json?{parse.urlencode([('apikey', api_key)])}'
-print(url)
+def get_events(city=None, date=None):
+    #obtains list of events based on search filters
+    url=f'{stem}.json?{parse.urlencode([('apikey', key)])}'
+    if(city):
+        url=f'{url}&{parse.urlencode([('city', city)])}'
+    if(date):
+        url=f'{url}&{parse.urlencode([('localStartEndDateTime', date)])}'
+    response = requests.get(url)
+    pprint(response.text)
 
-requests.get(url)
+def get_event(event_id):
+    #returns info about specific event based on event id
+    url=f'{stem}/{event_id}.json?{parse.urlencode([('apikey', key)])}'
+    print(url)
+    #requests.get(url)
 
+if __name__ == '__main__':
+    get_events(city=['Los Angeles'])
 #GET /discovery/v2/events.json?apikey=LFxCDAtqoQJlw3r7AZpdQ0kThXIQrEA0&size=1 HTTP/1.1
 #Host: app.ticketmaster.com
 #X-Target-URI: https://app.ticketmaster.com
